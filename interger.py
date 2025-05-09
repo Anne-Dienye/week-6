7187c6f5433b1d94fd379d6d29f9e48954e79098
Anne-Dienye <dienyeucheawaji93@gmail.com>
2025-05-09 11:42:34 +0100

    Delete interger.py

--- interger.py
+++
@@ -1,9 +0,0 @@
-#coding
-def find_factors(n):
-    factors = [i for i in range(1, abs(n) + 1) if n % i == 0]
-    return factors
-
-# Test the function
-number = int(input("Enter an integer: "))
-factors = find_factors(number)
-print(f"The factors of {number} are: {factors}")
